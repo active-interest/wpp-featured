a802def9b8ed2b3090ed0d37712f65ac4cb099bf
David Higgins <higginsd@zoulcreations.com>
2013-05-11 09:39:04 -0400

    ...

--- core/WPP_Featured.php
+++ core/WPP_Featured.php
@@ -19,9 +19,6 @@
  * @author David Higgins <higginsd@zoulcreations.com>
  */
 
-define('__WPP_FEATURED', 'wpp_is_featured');
-define('__WPP_FEATURED_NONCE', 'wpp_featured_nonce');
-
 class WPP_Featured {
 	private static $_initialized = false;
 	private static $_settings = array();
@@ -34,7 +31,7 @@ class WPP_Featured {
 		if(!array_key_exists('meta_query', $addArgs) || !is_array($addArgs['meta_query'])) {
 			$addArgs['meta_query'] = array();
 		}
-		$addArgs['meta_query'][] = array('key' => postmetaAlias, 'compare' => '=', value => 'true');
+		$addArgs['meta_query'][] = array('key' => self::postmetaAlias, 'compare' => '=', value => 'true');
 		
 		$args = array_merge(array(
 			'numberposts' => $count,
@@ -62,13 +59,13 @@ class WPP_Featured {
 	}
 	
 	public static function manage_posts_columns($columns) {
-		return array_merge($columns, array(postmetaAlias => 'Featured'));
+		return array_merge($columns, array(self::postmetaAlias => 'Featured'));
 	}
 	
 	public static function manage_posts_custom_column($column, $post_id) {
 		switch($column) {
-			case postmetaAlias: {
-				$is_featured = get_post_meta($post_id, postmetaAlias, false);
+			case self::postmetaAlias: {
+				$is_featured = get_post_meta($post_id, self::postmetaAlias, false);
 				echo '<input type="checkbox" disabled', ( $is_featured ?  ' checked="checked"' : ''), '/>';
 			} break;
 		}
@@ -77,31 +74,32 @@ class WPP_Featured {
 	public static function save_post($post_id) {
 		if (!isset($_POST['post_type']))
 			return $post_id;
-
-		if (!wp_verify_nonce( $_POST[settingsNonceName], plugin_basename(__FILE__)))
-			return $post_id;
-
 		if (defined('DOING_AUTOSAVE') && DOING_AUTOSAVE) 
 			return $post_id;
-
-		if (!current_user_can( 'edit_post', $post_id))
+		if (!current_user_can('edit_post', $post_id))
 			return $post_id;
 
-		if (!isset($_POST[postmetaAlias])) {
+//		if(wp_is_post_revision($postId)) 
+//			return; //Check to make sure it is not a revision
+		if (!wp_verify_nonce($_POST[self::settingsNonceName], plugin_basename(__FILE__)))
 			return $post_id;
-		} 
+
+		delete_post_meta($post, self::postmetaAlias);
+
+		if (!isset($_POST[self::postmetaAlias]))
+			return $post_id;
 		
-		$is_featured = $_POST[postmetaAlias];
-		update_post_meta( $post_id, postmetaAlias, $is_featured);
+		$is_featured = $_POST[self::postmetaAlias];
+		update_post_meta($post_id, self::postmetaAlias, $is_featured);
 	}
 	
 	public static function post_submitbox_misc_actions() {
 		global $post;
-		$featured = get_post_meta($post->ID, postmetaAlias, true);
+		$featured = get_post_meta($post->ID, self::postmetaAlias, true);
         echo '<div class="misc-pub-section misc-pub-section-last" style="border-top: 1px solid #eee;">';
-        wp_nonce_field(plugin_basename(__FILE__), settingsNonceName);
-		echo '<label for="' . postmetaAlias . '" style="font-weight: bold;">Is Featured: </label>';
-		echo '<input name="' . postmetaAlias . '" type="checkbox" id="' . postmetaAlias . '" ' . (!empty($featured) ? 'checked="checked"' : '') . ' value="true" />';
+        wp_nonce_field(plugin_basename(__FILE__), self::settingsNonceName);
+		echo '<label for="' . self::postmetaAlias . '" style="font-weight: bold;">Is Featured: </label>';
+		echo '<input name="' . self::postmetaAlias . '" type="checkbox" id="' . self::postmetaAlias . '" ' . (!empty($featured) ? 'checked="checked"' : '') . ' value="true" />';
         echo '</div>';
 	}
 }
